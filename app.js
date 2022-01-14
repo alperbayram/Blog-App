f7354fad0d1eb236ef5e2861f5adb0a8fcec25c8
alperbayram <alperbayrm@hotmail.com>
2022-01-14 15:08:00 +0300

    npmejs

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-const port = 5000;
+//middlewares
+app.use(express.static('public'));
+
+//template engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.json(blog);
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`sunucu ${port} portunda oluşturuldu.`);
